f15c912ba979925a70dcd7e220cca7225edfdbef
Marvin Kang'ethe <marvin.kangethe@andela.com>
2016-12-10 23:23:22 +0300

    Final code refactor.

--- app/fellow.py
+++ app/fellow.py
@@ -2,14 +2,13 @@ from app.storage import Storage
 
 
 class Fellow(object):
-	"""docstring for Fellow"""
+	"""
+	docstring for Fellow object.
+	"""
 	def __init__(self,id_no=0):
-		self.id_no = id_no
-
-	"""A  fellow can be allocated either a living space or an office."""	
+		self.id_no = id_no	
 	"""
 	Check if the person of id number X has been allocated room
-	
 	"""	
 	def is_allocated(self,id_no = 0):
 		self.id_no = id_no
